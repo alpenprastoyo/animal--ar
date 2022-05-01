dede7a9ae4e359e29a92d85832e5423dc8044a04
unknown <alvanprastoyo@staff.uns.ac.id>
2022-05-01 22:12:54 +0700

    first upload

--- resources/views/pages/scan_qr.blade.php
+++ resources/views/pages/scan_qr.blade.php
@@ -4,23 +4,25 @@
 @endsection
 
 @section('custom-js')
-<script type="text/javascript" src="{{ asset('assets/qrcode/qrcodelib.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/qrcode/webcodecamjs.js') }}"></script>
-<script type="text/javascript">
-    var txt = "innerText" in HTMLElement.prototype ? "innerText" : "textContent";
-    var arg = {
-        resultFunction: function(result) {
-            var aChild = document.createElement('li');
-            aChild[txt] = result.format + ': ' + result.code;
-            // alert(result.code);
-            // console.log(result.code);
-            window.location.replace(result.code);
-            // document.querySelector('body').appendChild(aChild);
-        }
-    };  
-    var decoder = new WebCodeCamJS("canvas").init(arg).buildSelectMenu('select', 2); 
-    setTimeout(function(){ decoder.play(); }, 500); 
-</script>
+    <script type="text/javascript" src="{{ asset('assets/qrcode/qrcodelib.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/qrcode/webcodecamjs.js') }}"></script>
+    <script type="text/javascript">
+        var txt = "innerText" in HTMLElement.prototype ? "innerText" : "textContent";
+        var arg = {
+            resultFunction: function(result) {
+                var aChild = document.createElement('li');
+                aChild[txt] = result.format + ': ' + result.code;
+                // alert(result.code);
+                // console.log(result.code);
+                window.location.replace(result.code);
+                // document.querySelector('body').appendChild(aChild);
+            }
+        };
+
+
+        new WebCodeCamJS('canvas').buildSelectMenu(document.createElement('select'),
+            'environment|back').init(args).play();
+    </script>
 @endsection
 
 @section('content')
@@ -30,9 +32,9 @@
                 <canvas></canvas>
             </div>
             <h1 class="text-shadow page-heading"><i class="fa fa-arrow-circle-o-right"></i>Silahkan Scan QR Code</h1>
-           
+
         </div>
         <div class="decoration"></div>
-    
+
     </div>
 @endsection
