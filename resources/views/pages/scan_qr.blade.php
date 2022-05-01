49225aebe08cb393c4ecf04e9f220188c915186e
unknown <alvanprastoyo@staff.uns.ac.id>
2022-05-01 22:33:50 +0700

    first upload

--- resources/views/pages/scan_qr.blade.php
+++ resources/views/pages/scan_qr.blade.php
@@ -4,7 +4,38 @@
 @endsection
 
 @section('custom-js')
-    <script type="text/javascript" src="{{ asset('assets/qrcode/qrcodelib.js') }}"></script>
+
+    <script type="text/javascript" src="https://unpkg.com/html5-qrcode@2.2.1/html5-qrcode.min.js"></script>
+    <script>
+        function docReady(fn) {
+            // see if DOM is already available
+            if (document.readyState === "complete"
+                || document.readyState === "interactive") {
+                // call on next available tick
+                setTimeout(fn, 1);
+            } else {
+                document.addEventListener("DOMContentLoaded", fn);
+            }
+        }
+    
+        docReady(function () {
+            var resultContainer = document.getElementById('qr-reader-results');
+            var lastResult, countResults = 0;
+            function onScanSuccess(decodedText, decodedResult) {
+                if (decodedText !== lastResult) {
+                    ++countResults;
+                    lastResult = decodedText;
+                    // Handle on success condition with the decoded message.
+                    console.log(`Scan result ${decodedText}`, decodedResult);
+                }
+            }
+    
+            var html5QrcodeScanner = new Html5QrcodeScanner(
+                "qr-reader", { fps: 10, qrbox: 250 });
+            html5QrcodeScanner.render(onScanSuccess);
+        });
+    </script>
+    {{-- <script type="text/javascript" src="{{ asset('assets/qrcode/qrcodelib.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/qrcode/webcodecamjs.js') }}"></script>
     <script type="text/javascript">
         var txt = "innerText" in HTMLElement.prototype ? "innerText" : "textContent";
@@ -27,14 +58,16 @@
             // Nothing done yet? Launch actual camera
             decoder.play();
         }, 500);
-    </script>
+    </script> --}}
 @endsection
 
 @section('content')
     <div class="main">
         <div class="container">
             <div class="polaroid-effect half-bottom">
-                <canvas></canvas>
+                {{-- <canvas></canvas> --}}
+                <div id="qr-reader" style="width:500px"></div>
+                <div id="qr-reader-results"></div>
             </div>
             <h1 class="text-shadow page-heading"><i class="fa fa-arrow-circle-o-right"></i>Silahkan Scan QR Code</h1>
 
