2e73229780e339f6eb36ba3d79ddc3be8a3be49f
unknown <alvanprastoyo@staff.uns.ac.id>
2022-05-01 23:31:53 +0700

    first upload

--- public/assets/mobile/scripts/custom.js
+++ public/assets/mobile/scripts/custom.js
@@ -125,36 +125,36 @@ $(document).ready(function(){
 	}    
 	setTimeout(init_template, 0);
 	
-    $(function(){
-		'use strict';
-		var options = {
-			prefetch: true,
-			prefetchOn: 'mouseover',
-			cacheLength: 100,
-			scroll: true, 
-			blacklist: '.default-link' && '.show-gallery',
-			forms: 'contactForm',
-			onStart: {
-				duration:350, // Duration of our animation
-				render: function ($container) {
-				$container.addClass('is-exiting');// Add your CSS animation reversing class
-				$("#page-preloader").removeClass('hide-preloader');
-			}
-        },
-        onReady: {
-			duration: 50,
-			render: function ($container, $newContent) {
-				$container.removeClass('is-exiting');// Remove your CSS animation reversing class
-				$container.html($newContent);// Inject the new content
-				$("#page-preloader").addClass('hide-preloader');
-			}
-		},
-        onAfter: function($container, $newContent) {
-            setTimeout(init_template, 0)//Timeout required to properly initiate all JS Functions. 
-			$("#page-preloader").addClass('hide-preloader');
+    // $(function(){
+	// 	'use strict';
+	// 	var options = {
+	// 		prefetch: true,
+	// 		prefetchOn: 'mouseover',
+	// 		cacheLength: 100,
+	// 		scroll: true, 
+	// 		blacklist: '.default-link' && '.show-gallery',
+	// 		forms: 'contactForm',
+	// 		onStart: {
+	// 			duration:350, // Duration of our animation
+	// 			render: function ($container) {
+	// 			$container.addClass('is-exiting');// Add your CSS animation reversing class
+	// 			$("#page-preloader").removeClass('hide-preloader');
+	// 		}
+    //     },
+    //     onReady: {
+	// 		duration: 50,
+	// 		render: function ($container, $newContent) {
+	// 			$container.removeClass('is-exiting');// Remove your CSS animation reversing class
+	// 			$container.html($newContent);// Inject the new content
+	// 			$("#page-preloader").addClass('hide-preloader');
+	// 		}
+	// 	},
+    //     onAfter: function($container, $newContent) {
+    //         // setTimeout(init_template, 0)//Timeout required to properly initiate all JS Functions. 
+	// 		$("#page-preloader").addClass('hide-preloader');
 
-        }
-      };
-      var smoothState = $('#page-transitions').smoothState(options).data('smoothState');
-    });      
+    //     }
+    //   };
+    //   var smoothState = $('#page-transitions').smoothState(options).data('smoothState');
+    // });      
 });
